8df0f95fde24d1a74527291fc7a7d9a8d608edba
QinyangCai <70783175+QinyangCai@users.noreply.github.com>
2020-09-19 19:45:50 -0400

    applepicker

---
+++ Assets/Basket.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;     // This line enables 
+
+public class Basket : MonoBehaviour {
+    [Header("Set Dynamically")]
+    public Text           scoreGT;    
+
+    void Update () {
+        // Get the current screen position of the mouse from Input
+        Vector3 mousePos2D = Input.mousePosition;                           // 1
+
+        // The Camera's z position sets the how far to push the mouse into 3D
+        mousePos2D.z = -Camera.main.transform.position.z;                   // 2
+
+        // Convert the point from 2D screen space into 3D game world space
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint( mousePos2D );  // 3
+
+        // Move the x position of this Basket to the x position of the Mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+    }
+	void Start() {
+        // Find a reference to the ScoreCounter
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        // Get the Text Component of that GameO
+        scoreGT = scoreGO.GetComponent<Text>();
+        // Set the starting number of points to
+        scoreGT.text = "0";
+    }
+	  void OnCollisionEnter ( Collision coll){
+		  //Find out what hit this basket
+		  GameObject collidedWith = coll.gameObject;
+	  if ( collidedWith.tag == "Apple"){
+		  Destroy ( collidedWith);
+		  
+		  //Parse the text of the scoreGT in
+		  int score = int.Parse( scoreGT.text);
+		  score += 100;
+		  scoreGT.text = score.ToString();
+		    // Track the high score
+    if (score > HighScore.score) {
+        HighScore.score = score;
+    }
+	  }
+	  }
+}
---
+++ Assets/HighScore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;    // Remember, we need t
+public class HighScore : MonoBehaviour {
+    static public int    score = 1000;
+    void Awake() {                             
+        // If the PlayerPrefs HighScore already
+        if (PlayerPrefs.HasKey("HighScore")) {
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+        // Assign the high score to HighScore
+        PlayerPrefs.SetInt("HighScore", score);
+    }
+    void Update () {
+        Text gt = this.GetComponent<Text>();
+        gt.text = "High Score: "+score;
+        // Update the PlayerPrefs HighScore if 
+        if (score > PlayerPrefs.GetInt("HighScore")){
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+    }
+}
